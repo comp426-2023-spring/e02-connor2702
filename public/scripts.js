e6bd6e3881d445338124363c5202d2de8c8b75a5
root <root@UNC-PF2A3Z3S>
2023-05-09 15:29:01 -0400

    Finished build v1.0

--- public/scripts.js
+++ public/scripts.js
@@ -5,6 +5,7 @@ function showHideShots() {
     let rps = document.getElementById('rps');
     let rpsls = document.getElementById('rpsls');
     let check = document.getElementById('opponent');
+
 // Check if the checkbox is checked and show or hide options accordingly
     if (check.checked == true && rps.checked == true) {
 // Here, instead of just showing all of the options, use similar logic to 
@@ -25,34 +26,75 @@ function showHideShots() {
 // radio buttons. 
 function startOver () {
     document.getElementById('userinput').reset();
+    $('game-player').hide()
+    $('game-opponent').hide()
+    $('game-result').hide()
     showHideShots();
 }
 
 async function playGame () {
     // Get which game is being played based on the value in the form
     let game = $('input[type=radio][name=game]:checked').val();
-    // Get which shot is being played based on the value in the form
-    let shot = $('input[type=radio][name=shot]:checked').val();
+    // Get option for playing versus opponent or not
+    let opponent = document.getElementById('opponent');
     // Identify the base URL based on browser information
     let baseurl = window.location.href + 'app/'
     // Log the base URL
     console.log(baseurl)
-    // This constructs a URL for the opponent option ONLY. To incorporate
-    // the other option, you can use a conditional to change the URL based
-    // on what is selected. You could also write separate functions, or use
-    // a conditional somewhere above in this function to construct the 
-    // correct URL
-    let url = baseurl + game + '/play/' + shot
-    // Log the full URL
-    console.log(url)	
+    // Get and display results based on if opponent is selected or not
+    if (opponent.checked == true) {
+        $('game-player').hide()
+        $('game-opponent').hide()
+        $('game-result').hide()
+        // Get which shot is being played based on the value in the form
+        let shot = $('input[type=radio][name=shot]:checked').val();
+        let url = baseurl + game + '/play/' + shot
 
-    let response = await fetch(url)
-    let result = await response.json()
-    // Log the result
-    console.log(result)
+        // Log the full URL
+        console.log(url)	
+
+        let response = await fetch(url)
+        let result = await response.json()
+        // Log the result
+        console.log(result)
+
+        // Display the result
+        document.getElementById('outputp').innerHTML = result.player
+        document.getElementById('outputo').innerHTML = result.opponent
+        document.getElementById('outputr').innerHTML = result.result
+
+        $('game-player').show()
+        $('game-opponent').show()
+        $('game-result').show()
+    } else {
+        $('game-player').hide()
+        $('game-opponent').hide()
+        $('game-result').hide()
+        let url = baseurl + game + '/play/'
+
+        // Log the full URL
+        console.log(url)	
+
+        let response = await fetch(url)
+        let result = await response.json()
+        // Log the result
+        console.log(result)
+
+        // Display the result
+        document.getElementById('outputp').innerHTML = result.player
+
+        $('game-player').show()
+    }
+
+
+
+
     // Here you should include code that uses the DOM API or jQuery to 
     // manipulate another block of HTML in the interface to display the 
     // results in some way.
-
-
+    // fix + delete - use html like before to display result
+    //steps
+    // 1. hide options by default
+    // 2. get them to display results accordingly
+    // do the above based on each if statement above separately
 }
